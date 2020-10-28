3c9a037d3811d4d7799cbd54618cba0eb0daeb44
Ysset <KamDenech@yandex.ru>
2020-10-28 16:59:03 +0300

    Fix path to imageUrl

--- src/Components/Card/hotFeedCard.js
+++ src/Components/Card/hotFeedCard.js
@@ -3,8 +3,6 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import {NavLink} from "react-router-dom";
-import Image from "cloudinary-react";
-import Transformation from "cloudinary-react";
 
 const handleCard = props => {
     console.log(props.index)
@@ -58,7 +56,7 @@ const HotFeedCard = props => {
                                 key={index}
                             >
                                 <Paper className={classes.paper}>
-                                    <img alt={'interesting line'} src={content.coverForHotFeedUrl} className={classes.img}/>
+                                    <img alt={'interesting line'} src={content.coverUrl} className={classes.img}/>
                                 </Paper>
                                 <h3 style={{
                                     margin: 0,
